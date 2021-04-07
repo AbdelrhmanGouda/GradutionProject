170aab42d9cae711a7dfe9ce1be9c738261a9bd1
abdelrhmanfarid <abdofarid2030@gmail.com>
2021-04-08 01:29:24 +0200

    test
    6666

--- app/src/main/java/com/example/graduationproject/Sign/Utils.java
+++ app/src/main/java/com/example/graduationproject/Sign/Utils.java
@@ -1,7 +1,8 @@
 package com.example.graduationproject.Sign;
 
 public class Utils {
-
+	
+	//6666
     //Email Validation pattern
     public static final String regEx = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
 
