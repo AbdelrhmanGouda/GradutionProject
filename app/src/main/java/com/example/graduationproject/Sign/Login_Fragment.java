ab8954c6d901e4093a414f6cc65cfe43dd8ce57b
ahmed98Osama <61237536+ahmed98Osama@users.noreply.github.com>
2021-04-21 03:29:50 +0200

    Facebook

--- app/src/main/java/com/example/graduationproject/Sign/Login_Fragment.java
+++ app/src/main/java/com/example/graduationproject/Sign/Login_Fragment.java
@@ -31,6 +31,16 @@ import androidx.fragment.app.FragmentManager;
 
 import com.example.graduationproject.Activities.MainActivity;
 import com.example.graduationproject.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.internal.ImageRequest;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -40,6 +50,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -48,6 +59,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import org.json.JSONObject;
+
+import java.net.URI;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -60,6 +74,7 @@ public class Login_Fragment extends Fragment implements OnClickListener {
     private static LinearLayout loginLayout;
     private static Animation shakeAnimation;
     private static FragmentManager fragmentManager;
+    private static LoginButton loginButtonF ;
     FirebaseStorage storage;
     StorageReference storageReference;
     User userData = new User();
@@ -68,10 +83,18 @@ public class Login_Fragment extends Fragment implements OnClickListener {
 
 
 
+    private CallbackManager mCallbackManager;
+
+    private static final String FTAG = "FacebookLogin";
+    private static final int RC_SIGN_IN = 12345;
+
 
     public GoogleSignInClient mGoogleSignInClient;
     public FirebaseAuth mAuth;
     public FirebaseUser user;
+
+
+
 
     public Login_Fragment() {
 
@@ -89,6 +112,9 @@ public class Login_Fragment extends Fragment implements OnClickListener {
                 .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+
+        mCallbackManager = CallbackManager.Factory.create();
 
         mAuth = FirebaseAuth.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
@@ -115,6 +141,8 @@ public class Login_Fragment extends Fragment implements OnClickListener {
     private void initViews() {
         fragmentManager = getActivity().getSupportFragmentManager();
 
+        loginButtonF = (LoginButton) view.findViewById(R.id.login_button);
+        loginButtonF.setFragment(Login_Fragment.this);
         emailid = (EditText) view.findViewById(R.id.login_emailid);
         password = (EditText) view.findViewById(R.id.login_password);
         loginButton = (Button) view.findViewById(R.id.loginBtn);
@@ -128,6 +156,9 @@ public class Login_Fragment extends Fragment implements OnClickListener {
         // Load ShakeAnimation
         shakeAnimation = AnimationUtils.loadAnimation(getActivity(),
                 R.anim.shake);
+
+
+
 
         // Setting text selector over textviews
         @SuppressLint("ResourceType") XmlResourceParser xrp = getResources().getXml(R.drawable.text_selector);
@@ -149,6 +180,8 @@ public class Login_Fragment extends Fragment implements OnClickListener {
         signUp.setOnClickListener(this);
         googleBtn.setOnClickListener(this);
         faceBookBtn.setOnClickListener(this);
+        loginButtonF.setOnClickListener(this);
+
 
 
         // Set check listener over checkbox for showing and hiding password
@@ -205,13 +238,14 @@ public class Login_Fragment extends Fragment implements OnClickListener {
 
                 break;
             case R.id.google:
+
                 signIn();
 
                 break;
 
             case R.id.facebook:
-
-
+                loginButtonF.performClick();
+                signInFacebook();
                 break;
 
 
@@ -305,13 +339,13 @@ public class Login_Fragment extends Fragment implements OnClickListener {
         /*-------- Check if G user is already logged in or not--------*/
         if (user != null) {
             String name = user.getDisplayName();
-            Toast.makeText(getActivity(), "Google Login Success." + name,
+            Toast.makeText(getActivity(), "Google or Facebook Login Success." + name,
                     Toast.LENGTH_SHORT).show();
 
             startActivity(new Intent(getActivity(),MainActivity.class));
 
         } else {
-            Toast.makeText(getActivity(), "Welcome , G none :)",
+            Toast.makeText(getActivity(), "Welcome , G , F none :)",
                     Toast.LENGTH_SHORT).show();
         }
 
@@ -337,13 +371,19 @@ public class Login_Fragment extends Fragment implements OnClickListener {
                     String personEmail = account.getEmail();
                     String personId = account.getId();
                     Uri personPhoto = account.getPhotoUrl();
-                    saveGoogleAuthToRealFirebase(personId,personName,personEmail,personPhoto);
+                    saveGoogleAndFacebookAuthToRealFirebase(personId,personName,personEmail,personPhoto);
                 }
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
                 Log.w(TAG, "Google sign in failed", e);
             }
+        }else {
+
+            // Pass the activity result back to the Facebook SDK
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
+
         }
+
     }
     // [END onactivityresult]
 
@@ -376,7 +416,7 @@ public class Login_Fragment extends Fragment implements OnClickListener {
     }
     // [END signin]
 
-    public void saveGoogleAuthToRealFirebase(String uid, String name, String email,Uri uri) {
+    public void saveGoogleAndFacebookAuthToRealFirebase(String uid, String name, String email, Uri uri) {
 
         userData.setEmail(email);
         userData.setUri(uri);
@@ -385,7 +425,6 @@ public class Login_Fragment extends Fragment implements OnClickListener {
         ///userData.setPassword(password);
         userData.setName(name);
         userData.setid(uid);
-
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("Users").child(uid);
@@ -399,6 +438,105 @@ public class Login_Fragment extends Fragment implements OnClickListener {
         myRef.child("name").setValue(userData.getName());
 
 
+    }
+
+    private void signInFacebook(){
+        // [START initialize_fblogin]
+        // Initialize Facebook Login button
+
+
+
+
+        loginButtonF.setPermissions("email", "public_profile");
+        loginButtonF.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(FTAG, "facebook:onSuccess:" + loginResult);
+
+                handleFacebookAccessToken(loginResult.getAccessToken());
+                //Use GraphApi to get the information into the app.
+                GraphRequest request = GraphRequest.newMeRequest(
+                        //pass two parameter
+                        loginResult.getAccessToken(),                       //one is the current token
+                        new GraphRequest.GraphJSONObjectCallback()          //2nd is grahJSONObject callback
+                        {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("facebookdata", response.toString());
+
+                                // Application code
+                                try {
+                                    String obj = object.toString();                     //get complete JSON object refrence.
+                                    String name = object.getString("name");                 //get particular JSON Object
+                                    String email = object.getString("email");
+                                    //String birthday = object.getString("birthday"); // 01/31/1980 format
+                                    String id = object.getString("id");
+                                    //String gender = object.getString("gender");
+                                    String profileURL = "";
+                                    //Log.v("facebookdata1", loginResult.getAccessToken().toString());
+                                    if (Profile.getCurrentProfile() != null) {                  //add this check because some people don't have profile picture
+                                        profileURL = ImageRequest.getProfilePictureUri(Profile.getCurrentProfile().getId(), 400, 400).toString();
+                                        //after getting the profile url you can easily set this to image view using Glide or retrofit library . simple :)
+                                    }
+
+                                    Log.v("facebookdata2",obj);
+                                    Log.v("facebookdata","id: "+id);
+                                    Log.v("facebookdata","name: "+name);
+                                    Log.v("facebookdata","email: "+email);
+                                   // Log.v("facebookdata","birthday: "+birthday);
+                                    //Log.v("facebookdata","gender: "+gender);
+                                    Log.v("facebookdata","profileURL: "+profileURL);
+                                    Uri uri = Uri.parse(profileURL);
+
+                                    saveGoogleAndFacebookAuthToRealFirebase(id,name,email,uri);
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");                    //set these parameter
+                request.setParameters(parameters);
+                request.executeAsync();                                 //exuecute task in seprate thread
+
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(FTAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(FTAG, "facebook:onError", error);
+            }
+        });
+        // [END initialize_fblogin]
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(FTAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(FTAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(FTAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
     }
 
 
