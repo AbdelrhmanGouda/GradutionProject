cc7def993784112e5bbfc93544162698d17a5601
ahmed98Osama <61237536+ahmed98Osama@users.noreply.github.com>
2021-05-05 18:25:57 +0200

    test

--- app/src/main/java/com/example/graduationproject/Sign/Utils.java
+++ app/src/main/java/com/example/graduationproject/Sign/Utils.java
@@ -2,7 +2,7 @@ package com.example.graduationproject.Sign;
 
 public class Utils {
 
-    //Email Validation pattern
+    //Email Validation pattern..
     public static final String regEx = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
 
     //Fragments Tags
