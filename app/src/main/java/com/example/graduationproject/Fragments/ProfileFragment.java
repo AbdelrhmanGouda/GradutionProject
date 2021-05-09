4fcb4407e0e4ce033eb2b72796fa6fdfd63efb58
Gouda <abdelrahman.m.goda@gmail.com>
2021-05-09 03:44:13 +0200

    Profile Fragment firebase

--- app/src/main/java/com/example/graduationproject/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/graduationproject/Fragments/ProfileFragment.java
@@ -1,7 +1,9 @@
 package com.example.graduationproject.Fragments;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -13,6 +15,15 @@ import android.widget.TextView;
 
 import com.example.graduationproject.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class
 
@@ -22,13 +33,14 @@ ProfileFragment extends Fragment implements View.OnClickListener {
     private TextView profileName,therapistNum,friendsNum,heartNum;
     private ImageView profilePic;
     private FloatingActionButton floatingEditProfileBtn;
+    public FirebaseAuth auth ;
+    String userName,userImage,friendsNumber,heartNumber;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_profile, container, false);
         setHasOptionsMenu(false);
-
         profileName=view.findViewById(R.id.profile_name);
         therapistNum=view.findViewById(R.id.profile_therapist_num);
         friendsNum=view.findViewById(R.id.profile_friends_num);
@@ -36,8 +48,81 @@ ProfileFragment extends Fragment implements View.OnClickListener {
         profilePic=view.findViewById(R.id.profile_image);
         floatingEditProfileBtn =view.findViewById(R.id.floating_edit_profile_btn);
         floatingEditProfileBtn.setOnClickListener(this);
+        auth=FirebaseAuth.getInstance();
+        dataBase();
 
         return view;
+    }
+
+    private void dataBase() {
+        FirebaseUser firebaseUser = auth.getCurrentUser();
+        final String id =firebaseUser.getUid();
+        Query query = FirebaseDatabase.getInstance().getReference().child("Users").child(id);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot!= null){
+                    if (snapshot.exists()&& snapshot.getChildrenCount()>0&&snapshot.getValue().toString().length()>0){
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                            userName=snapshot.child("name").getValue(String.class);
+                            userImage=snapshot.child("uri").getValue(String.class);
+                            profileName.setText(userName);
+                            Picasso.get().load(userImage).into(profilePic);
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        Query query1 = FirebaseDatabase.getInstance().getReference().child("Profiles").child("Friends").child("FriendsNumber").child(id);
+        query1.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot!= null){
+                    if (snapshot.exists()&& snapshot.getChildrenCount()>0&&snapshot.getValue().toString().length()>0){
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                            friendsNumber=snapshot.child("Friends").getValue(String.class);
+                            friendsNum.setText(friendsNumber);
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        Query query2 = FirebaseDatabase.getInstance().getReference().child("Profiles").child("Appreciate").child("Likes").child(id);
+        query2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot!= null){
+                    if (snapshot.exists()&& snapshot.getChildrenCount()>0&&snapshot.getValue().toString().length()>0){
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                            heartNumber=snapshot.child("Appreciate").getValue(String.class);
+                            heartNum.setText(heartNumber);
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 
 
