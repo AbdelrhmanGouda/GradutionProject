50532eb4066a30e981fa5f07b2e0e537b8f3d5c5
Gouda <abdelrahman.m.goda@gmail.com>
2021-05-22 00:17:10 +0200

    friend request Fragment firebase

--- app/src/main/java/com/example/graduationproject/Adapter/FriendRequestTabAdapter.java
+++ app/src/main/java/com/example/graduationproject/Adapter/FriendRequestTabAdapter.java
@@ -2,9 +2,12 @@ package com.example.graduationproject.Adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.graduationproject.Data.FriendListData;
 import com.example.graduationproject.Data.FriendRequestTabData;
 import com.example.graduationproject.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,21 +25,27 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class FriendRequestTabAdapter extends RecyclerView.Adapter<FriendRequestTabAdapter.FriendRequestTabAdapterViewHolder> {
-    List<FriendRequestTabData> friendRequestTabDataList;
+    List<FriendListData> friendListDataList;
     Context context;
-    String id,appreciateNumber,friendsNumber,uName,uri;
-    FirebaseUser firebaseUser;
+    String id,appreciateNumber,userName,userImage,friendName,friendImage;
     DatabaseReference profileRefrence;
+    FirebaseAuth auth;
+
 
     int i=0,j=0,imageResourceId;
 
-    public FriendRequestTabAdapter( List<FriendRequestTabData> friendRequestTabDataList ,Context context){
-        this.friendRequestTabDataList =friendRequestTabDataList;
+    public FriendRequestTabAdapter(List<FriendListData> friendListDataList , Context context){
+        this.friendListDataList =friendListDataList;
         this.context =context;
+        auth=FirebaseAuth.getInstance();
+        profileRefrence=FirebaseDatabase.getInstance().getReference();
+        userData();
+
     }
     @NonNull
     @Override
@@ -46,28 +56,81 @@ public class FriendRequestTabAdapter extends RecyclerView.Adapter<FriendRequestT
 
     @Override
     public void onBindViewHolder(@NonNull FriendRequestTabAdapterViewHolder holder, int position) {
-        holder.name.setText(friendRequestTabDataList.get(position).getName());
+        id =friendListDataList.get(position).getId();
+        friendName=friendListDataList.get(position).getName();
+        friendImage=friendListDataList.get(position).getUri();
+        holder.name.setText(friendName);
+        Picasso.get().load(friendImage).into(holder.imageView);
+        holder.confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFriends();
+                deleteFirendRequest();
+
+            }
+        });
+        holder.cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteFriends();
+                deleteFirendRequest();
+            }
+        });
+
+
     }
 
     @Override
     public int getItemCount() {
-        return friendRequestTabDataList.size();
+        return friendListDataList.size();
     }
 
     public class FriendRequestTabAdapterViewHolder extends RecyclerView.ViewHolder {
         TextView name;
+        ImageView imageView;
+        Button confirm,cancel;
         public FriendRequestTabAdapterViewHolder(@NonNull View itemView) {
             super(itemView);
             name=itemView.findViewById(R.id.friend_request_tab_name);
+            imageView=itemView.findViewById(R.id.friend_request_tab_image);
+            confirm=itemView.findViewById(R.id.friend_request_tab_confirm_btn);
+            cancel=itemView.findViewById(R.id.friend_request_tab_delete_btn);
+
+
         }
     }
+    private void userData(){
+        FirebaseUser firebaseUser=auth.getCurrentUser();
+        Query query =FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseUser.getUid());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot!= null){
+                    if (snapshot.exists()&& snapshot.getChildrenCount()>0&&snapshot.getValue().toString().length()>0){
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                            userName = snapshot.child("name").getValue(String.class);
+                            userImage = snapshot.child("uri").getValue(String.class);
+                            Log.i("username", userName);
 
-    private void addFriends() {
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+  private void addFriends() {
         if(getApprciate()!=null){
             j=Integer.valueOf(getApprciate());
         }
-
-        FriendListData data1=new FriendListData(userImage,userName,id,true);
+      FirebaseUser firebaseUser=auth.getCurrentUser();
+      FriendListData data=new FriendListData(userImage,userName,firebaseUser.getUid(),true);
+        FriendListData data1=new FriendListData(friendImage,friendName,id,true);
         j++;
         profileRefrence.child("Friends").child("FriendsNumber").child(id).child("Friends").setValue(String.valueOf(j));
         profileRefrence.child("Friends").child("FriendsNumber").child(firebaseUser.getUid()).child("Friends").setValue(String.valueOf(j));
@@ -80,17 +143,20 @@ public class FriendRequestTabAdapter extends RecyclerView.Adapter<FriendRequestT
         if(getApprciate()!=null){
             j=Integer.valueOf(getApprciate());
         }
-
-
-
-        j--;
-        profileRefrence.child("Friends").child("FriendsNumber").child(id).child("Friends").setValue(String.valueOf(j));
+       FirebaseUser  firebaseUser=auth.getCurrentUser();
         profileRefrence.child("Friends").child("FriendsList").child(id).child(firebaseUser.getUid()).removeValue() ;
         profileRefrence.child("Friends").child("FriendsList").child(firebaseUser.getUid()).child(id).removeValue();
 
 
     }
+    private void deleteFirendRequest() {
+
+        FirebaseUser  firebaseUser=auth.getCurrentUser();
+        profileRefrence.child("FriendsRequest").child(firebaseUser.getUid()).child(id).removeValue();
+
+    }
     private String getApprciate(){
+
         Query query6 = FirebaseDatabase.getInstance().getReference().child("Profiles").child("Friends").child("FriendsNumber")
                 .child(id);
         query6.addListenerForSingleValueEvent(new ValueEventListener() {
--- app/src/main/java/com/example/graduationproject/Fragments/NotificationFriendRequestsTabFragment.java
+++ app/src/main/java/com/example/graduationproject/Fragments/NotificationFriendRequestsTabFragment.java
@@ -2,43 +2,73 @@ package com.example.graduationproject.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.graduationproject.Adapter.FriendRequestTabAdapter;
-import com.example.graduationproject.Adapter.NotificationTabAdapter;
+import com.example.graduationproject.Data.FriendListData;
 import com.example.graduationproject.Data.FriendRequestTabData;
-import com.example.graduationproject.Data.NotificationTabData;
 import com.example.graduationproject.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NotificationFriendRequestsTabFragment extends Fragment {
     private RecyclerView friendRequestTabRecyclerView;
-    private List<FriendRequestTabData> friendRequestTabDataList;
+    private List<FriendListData> friendListDataList;
     private FriendRequestTabAdapter friendRequestTabAdapter;
+    FirebaseAuth auth;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notification_friend_requests_tab, container, false);
+        auth=FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser=auth.getCurrentUser();
+        String id =firebaseUser.getUid();
         friendRequestTabRecyclerView=view.findViewById(R.id.friend_request_tab_recycler_view);
         friendRequestTabRecyclerView.setLayoutManager( new LinearLayoutManager(getContext()));
         friendRequestTabRecyclerView.setHasFixedSize(true);
-        friendRequestTabDataList =new ArrayList<>();
-        friendRequestTabDataList.add(new FriendRequestTabData("Ahmed Osama"));
-        friendRequestTabDataList.add(new FriendRequestTabData("Abdelrhman Gouda"));
-        friendRequestTabDataList.add(new FriendRequestTabData("Mohamed Nasser"));
-        friendRequestTabDataList.add(new FriendRequestTabData("Khalid Nabil"));
-        friendRequestTabDataList.add(new FriendRequestTabData("Al monofy"));
-        friendRequestTabAdapter =new FriendRequestTabAdapter(friendRequestTabDataList,getContext());
-        friendRequestTabRecyclerView.setAdapter(friendRequestTabAdapter);
+        friendListDataList =new ArrayList<>();
+
+
+        Query query = FirebaseDatabase.getInstance().getReference().child("FriendsRequest").child(id);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                friendListDataList.clear();
+                if (snapshot.exists()) {
+                    for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                      FriendListData friendListData = snapshot1.getValue(FriendListData.class);
+                        friendListDataList.add(friendListData);
+                        Log.i(friendListData.getName(), "list: ");
+
+
+                    }
+                    friendRequestTabAdapter =new FriendRequestTabAdapter(friendListDataList,getContext());
+                    friendRequestTabRecyclerView.setAdapter(friendRequestTabAdapter);
+                    friendRequestTabAdapter.notifyDataSetChanged();
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         return view;
     }
 }
